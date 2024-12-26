e61c92cbba478b9abdece497286d112717442fe1
Yash Pawar <yashpawarp@gmail.com>
2024-12-26 16:26:41 +0530

    update home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 import CompanyLogoSlider from "@/components/home/companies-list";
 import { EventsSlider } from "@/components/home/events-slider"
+import HeroSection from "@/components/home/hero";
+import FeaturesGrid from "@/components/home/features";
+import FacultySection from "@/components/home/faculty-section";
 
 export default function Home() {
   return (
     <div>
-      <div className="min-h-screen">
       <EventsSlider />
-    </div>
       <CompanyLogoSlider />
+      <HeroSection/>
+      <FeaturesGrid/>
+      <FacultySection/>
     </div> 
   );
 }

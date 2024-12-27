506ee949b327fcc9ae00f735dd5ea51101c35785
sujay-deshpande <sujaydeshpande2406@gmail.com>
2024-12-27 10:50:18 +0530

    Addition of the pages title into the pages section

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,16 @@
 import { Footer } from "@/components/footer";
 import Navbar from "@/components/Navbar";
 import "@/app/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: { Component: any; pageProps: any }) {
+    const pageTitle = "I2IC - "+Component.name || "Default Title";
+    // console.log(Component)
     return (
         <div>
+            <Head>
+                <title>{pageTitle}</title> 
+            </Head>
             <Navbar />
             <Component {...pageProps} />
             <Footer />
